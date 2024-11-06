84351beed034aae11112bf22f9aae123e83da2f5
Anthony Gitau <gitau.ag@gmail.com>
2024-11-06 19:27:03 +0300

    add test and docker files

--- dist/src/controllers/home.controller.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.welcome = welcome;
-function welcome(req, res) {
-    return res.json({ message: "Welcome to test application." });
-}
